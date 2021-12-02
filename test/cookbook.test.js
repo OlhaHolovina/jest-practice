c3811f8a8ff65c5cec9aeece3614cdbfd72b53ea
Ola <ol.holovina@gmail.com>
2021-12-01 19:20:30 -0500

    Adding recipes test add

--- test/cookbook.test.js
+++ test/cookbook.test.js
@@ -3,7 +3,11 @@ const { Cookbook } = require('../src/cookbook');
 describe('Cookbook', () => {
   describe('Adding recipes', () => {
     test('should allow a recipe to be added', () => {
+      const myCookbook = new Cookbook();
 
+      myCookbook.addRecipe('cookie', ['eggs', 'water', 'flour', 'sugar']);
+
+      expect(myCookbook.recipes).toEqual({cookie: ['eggs', 'water', 'flour', 'sugar']})
     });
   });
 
