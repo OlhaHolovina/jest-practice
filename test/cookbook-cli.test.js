fde747324eb2f05e75a63386b0bafdc23b5cac55
Ola <ol.holovina@gmail.com>
2021-12-05 07:04:26 -0500

    Adding recipes test add

--- test/cookbook-cli.test.js
+++ test/cookbook-cli.test.js
@@ -4,7 +4,12 @@ const { CookbookCli } = require('../src/cookbook-cli');
 describe('CookbookCli', () => {
   describe('Adding recipes', () => {
     test('should accept the recipe information and display the correct message', () => {
+      const myCookBook = new Cookbook();
+      const myCookBookCli = new CookbookCli(myCookBook);
 
+      const message = myCookBookCli.add('hotdog', ['bread', 'meat']);
+
+      expect(message).toEqual(`Successfully added the following recipe: hotdog`)
     });
   });
 
