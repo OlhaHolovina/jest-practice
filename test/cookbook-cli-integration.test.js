0eb7e38832129cf34e6c6a8b35bdc7c363fe8782
Ola <ol.holovina@gmail.com>
2021-12-05 07:33:02 -0500

    CookbookCli integration suite add

---
+++ test/cookbook-cli-integration.test.js
@@ -0,0 +1,34 @@
+const { Cookbook } = require('../src/cookbook');
+const { CookbookCli } = require('../src/cookbook-cli');
+
+describe('CookbookCli integration', () => {
+  describe('Listing recipes', () => {
+    test('should allow the recipes to be listed', () => {
+
+    });
+  });
+
+  describe('Adding recipes', () => {
+    test('should accept the recipe information and add it to the CookBook', () => {
+
+    });
+  });
+
+  describe('Retrieving a recipe', () => {
+    test('should display the ingredients required to make the specified recipe', () => {
+
+    });
+  });
+
+  describe('Deleting a recipe', () => {
+    test('should accept the recipe name and remove the specific recipe form the Cookbook', () => {
+
+    });
+  });
+
+  describe('Incorrect command', () => {
+    test('should return a warning message if there is no such command', () => {
+
+    });
+  });
+});
